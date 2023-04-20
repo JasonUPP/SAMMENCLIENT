66ab79c0181822742bec45de86d3a21358eb7c55
Jason B <jasonbauelosrodriguez@yahoo.com.mx>
2023-04-19 23:18:24 -0600

    fix env url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
     title: 'Local Environment Heading',
-    apiURL: 'http://localhost:7002/'
+    apiURL: 'https://localhost:7002/'
     // apiURL: 'http://localhost:9095/' --Prod
   };
